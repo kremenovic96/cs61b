6dd0c522c3bd8e6703aa6100b098846f7700d096
Ranko <ranko.kremenovic@gmail.com>
2016-04-19 17:47:57 +0200

    commited non-finished  proj0

---
+++ proj0/NBody.java
@@ -0,0 +1,19 @@
+class NBody
+{
+
+public static double readRadius(String location)
+  {
+ In in = new In(location);
+   double radius=in.readDouble();
+  radius=in.readDouble();
+   return radius;
+   
+  }
+public static double[] readPlanets(String location)
+   {
+     In in = new In(location);
+ 
+
+   }
+
+}
---
+++ proj0/TestPlanet.java
@@ -0,0 +1,15 @@
+// wrote by me :)
+class TestPlanet
+{
+ public static void main (String args[])
+ {
+  checkCorrectness();
+
+ }
+public void checkCorrectness();
+ Pla
+
+
+
+
+}
