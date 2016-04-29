ee55a9573b2fece0c54ddbf27ed8445daa46dc20
kremenovic96 <ranko.kremenovic@gmail.com>
2016-04-29 23:20:48 +0200

    Added files for pro1a

---
+++ proj1/ArrayDeque.java
@@ -0,0 +1,22 @@
+public void addFirst( item){
+
+}
+public void addLast ( item){
+
+}
+public boolean isEmpty(){
+
+}
+public int size(){
+
+}
+public void printDeque()}
+
+}
+public item removeFirst(){
+
+}
+public item get(int index){
+
+}
+
---
+++ proj1/LinkedListDeque.java
@@ -0,0 +1,33 @@
+class LinkedListDeque{
+
+public LinkedListDeque(){
+
+}
+public Item getRecursive(int index){
+
+}
+public void addFirst( item){
+
+}
+public void addLast ( item){
+
+}
+public boolean isEmpty(){
+
+}
+public int size(){
+
+}
+public void printDeque()}
+
+}
+public Item removeFirst(){
+
+}
+
+public Item get(int index){
+
+}
+
+}
+
