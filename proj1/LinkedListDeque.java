3e3f3f87bd06a0ce372bc03659fb30f68c7e5eab
kremenovic96 <ranko.kremenovic@gmail.com>
2016-05-02 18:12:18 +0200

    working on proj1 ..

--- proj1/LinkedListDeque.java
+++ proj1/LinkedListDeque.java
@@ -1,31 +1,87 @@
-class LinkedListDeque{
+public class LinkedListDeque<Blorp> {
+ public class Node{
+  public Blorp head;
+  public Node next;
+  public Node prev;
+   public Node(Node p, Blorp h, Node n) {
+prev = p;
+head = h;
+next = n;
+}
+}
+ private Node sentinel;
+ private int size;
+ public LinkedListDeque() {
+ size = 0;
+ sentinel = new Node(null,null,null);
+ sentinel.prev = sentinel;
+ sentinel.next = sentinel;
+}
+ public Blorp getRecursive(int index) {
+  return null;
+}
+ public void addFirst(Blorp i) {
+ 
+ //Node oldFirst = sentinel.next;
+ /*cant create (maybe) first node when there is no nodes (cant retain first node)*/
+ Node firstNode = new Node(sentinel,i,sentinel.next);
+ sentinel.next = firstNode;
+ //sentinel.prev = firstNode;
+ if (size>1) {
+sentinel.next.next.prev = firstNode;
+}
+ size += 1;
+}
+// maybe the bad guy is this
+ public void addLast (Blorp i) {
 
-public LinkedListDeque(){
+
+ sentinel.prev.next = new Node(sentinel.prev,i,sentinel);
+ sentinel.prev = sentinel.prev.next;
+ size += 1;
+}
+ public boolean isEmpty() {
+ if (size == 0) {
+ return true;
+}
+return false;
+}
+ public int size() {
+ return size;
+}
+ public void printDeque() {
+ Node beginning = sentinel.next;
+ while (beginning != sentinel) {
+  System.out.print(beginning.head + " ");
+   beginning = beginning.next;
+}
 
 }
-public Item getRecursive(int index){
+//}
+ public Blorp removeFirst() {
+ if (sentinel.next == sentinel) {
+ return null;
 
 }
-public void addFirst( item){
+ 
+ Node first = sentinel.next;
+ first.next.prev = sentinel;
+ sentinel.next = first;//.next;
+ size -= 1;
 
-}
-public void addLast ( item){
-
-}
-public boolean isEmpty(){
-
-}
-public int size(){
-
-}
-public void printDeque()}
-
-}
-public Item removeFirst(){
-
+ return first.head;
 }
 
-public Item get(int index){
+public Blorp get(int index) {
+ if (index == 0) {
+  return sentinel.next.head;
+}
+ Node beginning=sentinel.next;
+ while (index != 0) {
+  beginning = beginning.next;
+index -= 1;
+}
+return beginning.head;
 
 }
 
--- proj1/LinkedListDequeTest.java
+++ proj1/LinkedListDequeTest.java
@@ -35,8 +35,8 @@ public class LinkedListDequeTest {
 	  * && is the "and" operation. */
 	public static void addIsEmptySizeTest() {
 		System.out.println("Running add/isEmpty/Size test.");
-		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-		/*
+		//System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+		//
 		LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
 		boolean passed = checkEmpty(true, lld1.isEmpty());
@@ -50,12 +50,12 @@ public class LinkedListDequeTest {
 
 		lld1.addLast("back");
 		passed = checkSize(3, lld1.size()) && passed;
-
+   
 		System.out.println("Printing out deque: ");
 		lld1.printDeque();
 
 		printTestStatus(passed);
-		*/
+		
 	}
 
 	/** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
@@ -63,8 +63,8 @@ public class LinkedListDequeTest {
 
 		System.out.println("Running add/remove test.");
 
-		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-		/*
+		//System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+		//
 		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 		// should be empty 
 		boolean passed = checkEmpty(true, lld1.isEmpty());
@@ -78,12 +78,13 @@ public class LinkedListDequeTest {
 		passed = checkEmpty(true, lld1.isEmpty()) && passed;
 
 		printTestStatus(passed);
-		*/
+		
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Running tests.\n");
 		addIsEmptySizeTest();
 		addRemoveTest();
+
 	}
 } 
