2a3038656cfe9ec6fc4fb4fa394afb3b87c2b032
ranko <ranko.kremenovic@gmail.com>
2016-06-17 06:46:48 +0200

    lab6 started Clorus.java

--- lab6/creatures/Clorus.java
+++ lab6/creatures/Clorus.java
@@ -1,7 +1,12 @@
 package creatures;
-
 import huglife.Creature;
 import java.awt.Color;
+import java.util.Map;
+
+import huglife.Action;
+import huglife.Direction;
+import huglife.Occupant;
+
 public class Clorus extends Creature {
     private int r;
     private int g;
@@ -32,12 +37,18 @@ public class Clorus extends Creature {
         energy -= 0.01;
     }
     public void attack(Creature c) {
-        
+        this.energy += c.energy();
+
+    }
+    public Clorus replicate() {
+        return new Clorus(this.energy *= 0.5);
+    }
+
+    public Action chooseAction(Map<Direction, Occupant> neighbors) {
+
 
 
     }
-
-
 
 
 
