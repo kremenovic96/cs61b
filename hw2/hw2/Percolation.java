e64c0cbdfd954870ffe53194155cf73d81e90f98
kremenovic96 <ranko.kremenovic@gmail.com>
2016-07-22 19:13:43 +0200

    Working throught hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -5,7 +5,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
    // int x; // got an idea but immediately forgot it, so lets name it just x.
     int rowLenght;
-    WeightedQuickUnionUF fullness;
+    WeightedQuickUnionUF sites;
     int TOP;
     int BOTTOM;
     int numOfopenSites;
@@ -18,7 +18,7 @@ public class Percolation {
         }
         TOP = N * N;
         BOTTOM = (N * N) + 1;
-        fullness =  new WeightedQuickUnionUF((N * N) + 2);
+        sites =  new WeightedQuickUnionUF((N * N) + 2);
 
 
         rowLenght = N;
@@ -29,8 +29,8 @@ public class Percolation {
             }
         }
         for(int i = 0; i < N; i++){
-            fullness.union(TOP, xyTo1d(0, i));
-            fullness.union(BOTTOM, xyTo1d(rowLenght - 1, i));
+            sites.union(TOP, xyTo1d(0, i));
+            sites.union(BOTTOM, xyTo1d(rowLenght - 1, i));
         }
     }
     public void open(int row, int col){
@@ -42,23 +42,23 @@ public class Percolation {
     grid[row][col] = true;
      if(row > 0) {
         if(grid[row][col] == grid[row -1][col]){
-            fullness.union(xyTo1d(row,col), xyTo1d(row -1, col));
+            sites.union(xyTo1d(row,col), xyTo1d(row -1, col));
 
         } }
-     if(row < rowLenght - 2){
+     if(row < rowLenght - 1){
          if(grid[row][col] == grid[row + 1][col]){
-            fullness.union(xyTo1d(row,col),xyTo1d(row + 1,col));
+            sites.union(xyTo1d(row,col),xyTo1d(row + 1,col));
 
         }}
       if(col > 0){
           if(grid[row][col] == grid[row][col - 1]){
-              fullness.union(xyTo1d(row,col), xyTo1d(row, col - 1));
+              sites.union(xyTo1d(row,col), xyTo1d(row, col - 1));
 
           }
       }
-      if(col < rowLenght - 2){
+      if(col < rowLenght - 1){
           if(grid[row][col] == grid[row][col + 1]){
-              fullness.union(xyTo1d(row,col), xyTo1d(row, col + 1));
+              sites.union(xyTo1d(row,col), xyTo1d(row, col + 1));
 
           }
       }
@@ -75,15 +75,14 @@ public class Percolation {
     public boolean isFull(int row, int col){
         if ( (rowLenght - 1 < row) || (rowLenght - 1 < col) || (row < 0) || (col < 0)) {
             throw new IndexOutOfBoundsException();
-
         }
-        return fullness.connected(TOP, xyTo1d(row,col));
+        return sites.connected(TOP, xyTo1d(row,col));
     }
     public int numberOfOpenSites(){
         return numOfopenSites;
     }
     public boolean percolates(){
-    return fullness.connected(TOP, BOTTOM);
+    return sites.connected(TOP, BOTTOM);
     }
     private int xyTo1d(int row, int col){
         return col + row * (rowLenght );
